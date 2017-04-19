4765cd84789850fb03cce330bb1f6bf83f009d4c
Erik Brandon Zapata <zapatabrandon@gmail.com>
2017-04-19 15:39:38 -0400

    Adds level loader and main menu functionality

---
+++ Assets/Scripts/EnemyAI/EnemyAI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // wander around a given area or just stand still depending on some condition
+    void Wander()
+    {
+
+    }
+
+    // follow 
+    void Follow()
+    {
+
+    }
+
+    // start the attack animation and create a hitting collider
+    void Attack()
+    {
+
+    }
+
+    // start the shooting animation and shoot a couple of bullets 
+    void Shoot()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Level/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // 0 is level 1
+    // 1 is level 2
+    // 2 is main menu
+    public void LoadLevel(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+}
