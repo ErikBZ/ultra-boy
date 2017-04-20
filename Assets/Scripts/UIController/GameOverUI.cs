1d21bacff544d5a53e285327db1839e6227c4db9
Thai Nguyen <Potato@Thais-iMac.home>
2017-04-19 23:52:49 -0400

    Font/UI changes

---
+++ Assets/Scripts/UIController/GameOverUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class GameOverUI : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+
+
+	public void Quit()
+	{
+		SceneManager.LoadScene ("Main Menu");
+
+	}
+
+	public void Retry()
+	{
+		Debug.Log ("Restart from checkpoint");
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+	}
+}
